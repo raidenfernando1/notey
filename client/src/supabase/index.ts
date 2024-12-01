f358d35100eb71ea31ea364196dad3cf7280fdac
Raiden Fernando <157260780+raidenfernando1@users.noreply.github.com>
2024-12-01 20:33:42 +0800

    fixed vercel env variables

--- client/src/supabase/index.ts
+++ client/src/supabase/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.VITE_SECRET_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SECRET_SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables');
